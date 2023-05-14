b58031645b105e467d192849a7e82d1c51e8b8e7
Flora-Amanuel <amanuelflora@gmail.com>
2023-05-14 00:55:51 -0500

    flora first project

---
+++ SpringLogin/src/main/java/com/service/LoginService.java
@@ -0,0 +1,20 @@
+package com.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dao.LoginRepository;
+import com.entity.Login;
+
+@Service
+public class LoginService {
+	
+@Autowired
+private LoginRepository repo;
+ 
+ public Login login(String username, String password) {
+ Login user = repo.findByUsernameAndPassword(username, password);
+  return user;
+ }
+
+}
